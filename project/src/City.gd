c99ae2df1338f0fdaf2cdb38e15ced7c580350aa
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-04-14 09:23:00 -0400

    Remove unneeded visibility notifiers

--- project/src/City.gd
+++ project/src/City.gd
@@ -16,18 +16,6 @@ onready var _highlight := $Highlight
 onready var _animation_player := $AnimationPlayer
 
 
-# Report whether this city is currently on screen enough to be selectable.
-# It is not enough to say that just a bit of it is onscreen, and hence
-# the use of multiple regions. A VisibilityNotifier2D triggers if any part
-# of it is visible, but we need multiple areas to be visible.
-func is_on_screen()->bool:
-	for notifier in $VisibilityNotifiers.get_children():
-		var sensor := notifier as VisibilityNotifier2D
-		if not sensor.is_on_screen():
-			return false
-	return true
-
-
 func _set_city_name(value:String)->void:
 	city_name = value
 	$Label.text = value
